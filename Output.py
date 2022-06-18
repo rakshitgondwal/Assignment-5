0777edb80dcf9c1eb3b6909685df90ae32985083
Rakshit Gondwal <98955085+rakshitgondwal@users.noreply.github.com>
2022-06-18 22:02:13 +0530

    Create Output.py

---
+++ Output.py
@@ -0,0 +1,224 @@
+*** Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32. ***
+*** Remote Python engine is active ***
+>>> 
+*** Remote Interpreter Reinitialized ***
+
+Welcome to my Program
+Kindly enter a string : Rakshit
+
+The string in reverse order : 
+tihskaR
+>>> 
+*** Remote Interpreter Reinitialized ***
+
+Welcome to my Program
+Kindly enter the lower limit of the divisibility range : 2
+Kindly enter the upper limit of the divisibility range : 4
+Kindly enter the number that is to be divided : 3
+
+Numbers divisible in the given range are as follows : 
+3
+>>> 
+*** Remote Interpreter Reinitialized ***
+
+Welcome to my Program
+Please enter the length of side 1 : 2
+Please enter the length of side 2 : 3
+Please enter the length of side 3 : 4
+
+Area of the triangle is :  2.9047375096555625
+>>> 
+*** Remote Interpreter Reinitialized ***
+
+Welcome to my Program
+
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
+>>> 
+*** Remote Interpreter Reinitialized ***
+
+Welcome to my Program
+Kindly enter the number of rows : 5
+A
+BC
+DEF
+GHIJ
+KLMNO
+>>> 
+*** Remote Interpreter Reinitialized ***
+
+Welcome to my Program
+
+Kindly enter the lower limit : 5
+Kindly enter the upper limit : 9
+
+Prime numbers between 5 and 9 are : 
+5
+7
+>>> 
+*** Remote Interpreter Reinitialized ***
+
+Welcome to my Program
+
+Between 1 and 500 the following numbers are multiple of 7 : 
+7
+14
+21
+28
+35
+42
+49
+56
+63
+70
+77
+84
+91
+98
+105
+112
+119
+126
+133
+140
+147
+154
+161
+168
+175
+182
+189
+196
+203
+210
+217
+224
+231
+238
+245
+252
+259
+266
+273
+280
+287
+294
+301
+308
+315
+322
+329
+336
+343
+350
+357
+364
+371
+378
+385
+392
+399
+406
+413
+420
+427
+434
+441
+448
+455
+462
+469
+476
+483
+490
+497
+
+Between 1 and 500 the following numbers are divisible by 11 : 
+11
+22
+33
+44
+55
+66
+77
+88
+99
+110
+121
+132
+143
+154
+165
+176
+187
+198
+209
+220
+231
+242
+253
+264
+275
+286
+297
+308
+319
+330
+341
+352
+363
+374
+385
+396
+407
+418
+429
+440
+451
+462
+473
+484
+495
+
+Between 1 and 500 the following numbers are both multiple of 7 and divisible by 11 : 
+77
+154
+231
+308
+385
+462
+>>> 
+*** Remote Interpreter Reinitialized ***
+
+Welcome to my Program
+Kindly enter the number : 5
+Kindly enter the number : 6
+Kindly enter the number : 7
+Kindly enter the number : 8
+Kindly enter the number : 9
+Kindly enter the number : 2
+Kindly enter the number : 3
+Kindly enter the number : 4
+Kindly enter the number : 5
+Kindly enter the number : 6
+Positive Numbers :  [5, 6, 7, 8, 9, 2, 3, 4, 5, 6]
+Negative Numbers :  []
+Even Numbers :  [6, 8, 2, 4, 6]
+Odd Numbers :  [5, 7, 9, 3, 5]
+Occurrence of each number in the List :  {5: 2, 6: 2, 7: 1, 8: 1, 9: 1, 2: 1, 3: 1, 4: 1}
+>>> 
+*** Remote Interpreter Reinitialized ***
+
+Welcome to my Program
+
+Kindly enter a string : rakshit
+
+Number of occurrences of each word is :  {'rakshit': 1}
+>>> 
